8d8a8bf442dd46d868c326477bfa66dcfbe26097
Loan <loantravail@gmail.com>
2022-02-10 09:46:26 +0100

    Start cas sIdentifier

--- src/controleur/ControlSIdentifier.java
+++ src/controleur/ControlSIdentifier.java
@@ -1,4 +1,19 @@
 package controleur;
 
+import modele.BDClient;
+import modele.BDPersonnel;
+import modele.ProfilUtilisateur;
+
 public class ControlSIdentifier {
+
+    //Attributs
+    private BDClient bdClient = BDClient.getInstance();
+    private BDPersonnel bdPersonnel = BDPersonnel.getInstance();
+
+    //methodes
+    public int sIdentifier(ProfilUtilisateur profilUtilisateur, String login, String mdp){
+
+
+        return 0;
+    }
 }
--- src/vueconsole/BoundarySIdentifierClient.java
+++ src/vueconsole/BoundarySIdentifierClient.java
@@ -1,4 +1,19 @@
 package vueconsole;
 
+import controleur.ControlSIdentifier;
+
 public class BoundarySIdentifierClient {
+    private ControlSIdentifier controlSIdentifier;
+
+    public BoundarySIdentifierClient(ControlSIdentifier c){
+        controlSIdentifier = c;
+    }
+
+    //methodes
+    public int sIdentifierClient(){
+
+
+
+        return 0;
+    }
 }
